ef384663e9f759fba9ef32d158304657977bc6b0
jaydennembot <jaydennembot54@gmail.com>
2026-04-22 16:56:34 +0100

    Add files via upload

--- space.py
+++ space.py
@@ -2,6 +2,8 @@ import pygame,pyautogui
 import random
 pygame.init()
 
+
+
 print(pyautogui.size())
 width,height=pyautogui.size()
 #width,height=600,600
@@ -16,20 +18,52 @@ rx,ry=width-220,height//2
 vx,vy=120,height//2
 space=pygame.image.load("space.jpg")
 space=pygame.transform.scale(space,(width,height))
-r1=pygame.Rect(rx,ry,120,140)
-v1=pygame.Rect(vx,vy,120,140)
-while True:
-   
+
+def draw(r1,v1):
     screen.blit(space,(0,0))
    
-    pygame.draw.rect(screen,"white",r1)
-    pygame.draw.rect(screen,"white",v1)
-    screen.blit(vampire,(v1.x,v1.y))
-    screen.blit(robot,(r1.x,r1.y))
-    for i in pygame.event.get():
-        print (i)
-        if i.type == pygame.QUIT :
-            pygame.quit()
+    # pygame.draw.rect(screen,"white",r1)
+    # pygame.draw.rect(screen,"white",v1)
+    screen.blit(vampire,(v1.x-40,v1.y))
+    screen.blit(robot,(r1.x-40,r1.y))
+
+def movement(r1,v1,button):
+    # print(button)
+    if button == pygame.K_w:
+        v1.y-=10
+    if button == pygame.K_a:
+        v1.x-=10
+    if button == pygame.K_s:
+        v1.y+=10
+    if button == pygame.K_d:
+        v1.x+=10
+        print("hello")
+    if button == pygame.K_UP:
+        r1.y-=10
+    if button == pygame.K_DOWN:
+        r1.y+=10
+    if button == pygame.K_LEFT:
+        r1.x-=10
+    if button == pygame.K_RIGHT:
+        r1.x+=10
     
 
-    pygame.display.update()
+
+def main():
+    r1=pygame.Rect(rx,ry,80,140)
+    v1=pygame.Rect(vx,vy,80,140)
+    while True:
+    
+        draw(r1,v1)
+        for i in pygame.event.get():
+            
+            if i.type == pygame.QUIT :
+                pygame.quit()
+        button=pygame.key.get_pressed()
+        print(button)
+        movement(r1,v1,button)
+        
+
+        pygame.display.update()
+
+main()
